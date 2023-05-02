f5523b5bc51fef139b91844bcb7cd9866407fe45
Agustin Lavarello <agustinlare@gmail.com>
2023-05-02 05:52:16 -0300

    added syslog

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"log/syslog"
 	"net/http"
 	"net/url"
 	"os"
@@ -22,14 +23,18 @@ func main() {
 	for {
 		if checkInternetConnection() {
 			if internetIsLost {
+				logToSyslog()
+
 				internetIsLost = false
 				internetReturnTime := time.Now()
 				internetLostDuration := internetReturnTime.Sub(internetLostTime)
+
 				if internetLostDuration.Minutes() >= 1 {
 					message := fmt.Sprintf("Internet connection is back after %v minutes", internetLostDuration.Minutes())
 					sendDiscordNotification(webhookUrl, message)
 				}
 			}
+
 		} else {
 			if !internetIsLost {
 				internetIsLost = true
@@ -53,14 +58,32 @@ func sendDiscordNotification(webhookUrl, message string) {
 	formData := url.Values{
 		"content": {message},
 	}
+
 	resp, err := http.PostForm(webhookUrl, formData)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Println(string(body))
+}
+
+func logToSyslog() {
+	syslog, err := syslog.New(syslog.LOG_INFO, "is-connect")
+	if err != nil {
+		log.Fatal("Failed to connect to syslog:", err)
+	}
+
+	defer syslog.Close()
+
+	_, err = syslog.Write([]byte("Site is unreacheble"))
+	if err != nil {
+		log.Fatal("Failed to write to syslog:", err)
+	}
 }
